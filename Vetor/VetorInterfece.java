459e8f952efba8227f6fb44478b292bdd157b761
Asaph Arruda <asapharruda@gmail.com>
2025-05-12 15:00:23 -0300

    Mud: nome do metodo da interface de remove para removeAtRank

--- Vetor/VetorInterfece.java
+++ Vetor/VetorInterfece.java
@@ -6,5 +6,5 @@ public interface VetorInterfece {
     public Object elementAtRank(int indice) throws ElemVazia; 
     public Object replaceAtRank(int indice, Object novo_elemento) throws ElemVazia;
     public void insertAtRank(int indice, Object novo_elemento) throws ElemVazia;
-    public Object remove(int indice) throws ElemVazia;
+    public Object removeAtRank(int indice) throws ElemVazia;
 }
