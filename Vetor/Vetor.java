4404201e3ce47c4784a9028002b78545bdafec69
Asaph Arruda <asapharruda@gmail.com>
2025-05-12 14:59:30 -0300

    Add: metodos de inserir, remover e printar

--- Vetor/Vetor.java
+++ Vetor/Vetor.java
@@ -22,15 +22,15 @@ public class Vetor implements VetorInterfece {
     }
 
     public int tamanhoUsado() {
-        return ultimo_Elemento - 1;
+        return ultimo_Elemento;
     }
 
-    // public boolean isEmpty() {
-    //     return tamanho
-    // }
+    public boolean isEmpty() {
+        return ultimo_Elemento == 0;
+    }
 
     public Object elementAtRank(int indice) throws ElemVazia{
-        if (indice >= tamanho) {
+        if (indice >= ultimo_Elemento || indice < 0) {
             throw new ElemVazia("Indice inválido");
         } else {
             return vetor[indice];
@@ -38,7 +38,7 @@ public class Vetor implements VetorInterfece {
     } 
 
     public Object replaceAtRank(int indice, Object novo_elemento) throws ElemVazia{
-        if (indice >= tamanho) {
+        if (indice >= ultimo_Elemento || indice < 0) {
             throw new ElemVazia("Indice inválido");
         } else {
             Object elemento_out = vetor[indice];
@@ -48,15 +48,54 @@ public class Vetor implements VetorInterfece {
     }
     
     public void insertAtRank(int indice, Object novo_elemento) throws ElemVazia{
-        if (indice >= tamanho) {
+        if (indice > ultimo_Elemento || indice < 0)  {
             throw new ElemVazia("Indice inválido");
         } else {
-            if (tamanho == ultimo_Elemento) {
+            if (ultimo_Elemento == tamanho) {
                 tamanho += fator_Crescimento;
-                if (fator_Crescimento == 0) {   
-                    tamanho = tamanho * 2;
-
+                if (fator_Crescimento == 0)  {
+                    tamanho *= 2;
                 }
+
+                Object vetor_temp[] = new Object[tamanho];
+                for (int i = 0; i < ultimo_Elemento; i++) {
+                    vetor_temp[i] = vetor[i];
+                }
+                vetor = vetor_temp;
+            }
+            for (int i = ultimo_Elemento; i > indice; i--) {
+                vetor[i] = vetor[i-1];
+            } 
+            vetor[indice] = novo_elemento;
+            ultimo_Elemento++;
+        }
+    }
+
+    public Object removeAtRank(int indice) throws ElemVazia {
+        if (indice >= ultimo_Elemento || indice < 0) {
+        throw new ElemVazia("Indice inválido");
+    } else {
+        Object elemento_out = vetor[indice];
+        for (int i = indice; i < ultimo_Elemento; i++) {
+            vetor[i] = vetor[i + 1];
+        } 
+        vetor[ultimo_Elemento - 1] = null;
+        ultimo_Elemento--;
+
+        return elemento_out;
+    
+        }
+    }
+
+    public void printar() {
+        if (isEmpty()) {
+            System.out.println("Vetor Vazio");
+        } else{
+            int contador = 0;
+
+            while (contador < ultimo_Elemento) {
+                System.out.print(String.format("[ %s ] -> ", vetor[contador]));
+                contador++;
             }
         }
     }
