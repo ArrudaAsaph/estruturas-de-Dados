057ef4fc602c5df5309ac7d240a615575a9c917f
ASAPH BONIFACIO GUIMARAES ARRUDA <20241014040010@ifrn.local>
2025-06-24 10:22:22 -0300

    metod

---
+++ arvoreABB/No.java
@@ -0,0 +1,41 @@
+package arvoreABB;
+
+public class No {
+    private int elemento;
+    private No filhoEsquerdo,  filhoDireito, pai;
+
+    
+    public No(int elemento, No pai) {
+        this.elemento = elemento;
+        this.pai = pai;
+        this.filhoDireito = null;
+        this.filhoEsquerdo = null;
+    }
+
+    public int getElemento() {
+        return elemento;
+    }
+    public void setElemento(int elemento) {
+        this.elemento = elemento;
+    }
+    public No getFilhoEsquerdo() {
+        return filhoEsquerdo;
+    }
+    public void setFilhoEsquerdo(No filhoEsquerdo) {
+        this.filhoEsquerdo = filhoEsquerdo;
+    }
+    public No getFilhoDireito() {
+        return filhoDireito;
+    }
+    public void setFilhoDireito(No filhoDireito) {
+        this.filhoDireito = filhoDireito;
+    }
+    public No getPai() {
+        return pai;
+    }
+    public void setPai(No pai) {
+        this.pai = pai;
+    }
+
+    
+}
