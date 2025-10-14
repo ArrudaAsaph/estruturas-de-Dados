456bc6293fc91a3252514646e4fffa322163f03f
Asaph Arruda <160978598+ArrudaAsaph@users.noreply.github.com>
2025-10-14 14:48:54 +0000

    :sparkles: feat: adiciona sobrinholonge e perto

--- rubro_negro/Parente.java
+++ rubro_negro/Parente.java
@@ -3,10 +3,14 @@ package rubro_negro;
 public class Parente {
     private No avo;
     private No tio;
+    private No sobrinhoPerto;
+    private No sobrinhoLonge;
 
     public Parente() {
         this.avo = null;
         this.tio = null;
+        this.sobrinhoPerto = null;
+        this.sobrinhoLonge = null;
     }
 
     public void setAvo(No avo) {
@@ -24,4 +28,21 @@ public class Parente {
         return tio;
     }
 
+    public No getSobrinhoPerto() {
+        return sobrinhoPerto;
+    }
+
+    public void setSobrinhoPerto(No sobrinhoPerto) {
+        this.sobrinhoPerto = sobrinhoPerto;
+    }
+
+    public No getSobrinhoLonge() {
+        return sobrinhoLonge;
+    }
+
+    public void setSobrinhoLonge(No sobrinhoLonge) {
+        this.sobrinhoLonge = sobrinhoLonge;
+    }
+
+    
 }
